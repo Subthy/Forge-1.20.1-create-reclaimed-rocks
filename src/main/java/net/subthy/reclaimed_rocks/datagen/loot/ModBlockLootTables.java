b2cff9de508d02c16fd5f71df64a0f656504a624
Kristers <kristers891@gmail.com>
2024-06-15 12:58:26 +0300

    fix

--- src/main/java/net/subthy/reclaimed_rocks/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/subthy/reclaimed_rocks/datagen/loot/ModBlockLootTables.java
@@ -54,8 +54,6 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         //Dolomite
         this.add(ModBlocks.DOLOMITE_SLAB.get(),
                 block -> createSlabItemTable(ModBlocks.DOLOMITE_SLAB.get()));
-        this.add(ModBlocks.DOLOMITE_PAVED_BRICK_SLAB.get(),
-                block -> createSlabItemTable(ModBlocks.DOLOMITE_PAVED_BRICK_SLAB.get()));
         this.add(ModBlocks.DOLOMITE_COBBLESTONE_SLAB.get(),
                 block -> createSlabItemTable(ModBlocks.DOLOMITE_COBBLESTONE_SLAB.get()));
         this.add(ModBlocks.DOLOMITE_POLISHED_SLAB.get(),
@@ -69,8 +67,6 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         // Limestone
         this.add(ModBlocks.WEATHERED_LIMESTONE_SLAB.get(),
                 block -> createSlabItemTable(ModBlocks.WEATHERED_LIMESTONE_SLAB.get()));
-        this.add(ModBlocks.WEATHERED_LIMESTONE_PAVED_BRICK_SLAB.get(),
-                block -> createSlabItemTable(ModBlocks.WEATHERED_LIMESTONE_PAVED_BRICK_SLAB.get()));
         this.add(ModBlocks.WEATHERED_LIMESTONE_COBBLESTONE_SLAB.get(),
                 block -> createSlabItemTable(ModBlocks.WEATHERED_LIMESTONE_COBBLESTONE_SLAB.get()));
         this.add(ModBlocks.WEATHERED_LIMESTONE_POLISHED_SLAB.get(),
@@ -80,12 +76,10 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         this.add(ModBlocks.WEATHERED_LIMESTONE_FANCY_BRICK_SLAB.get(),
                 block -> createSlabItemTable(ModBlocks.WEATHERED_LIMESTONE_FANCY_BRICK_SLAB.get()));
         this.add(ModBlocks.WEATHERED_LIMESTONE_LAYERED_SLAB.get(),
-                block -> createSlabItemTable(ModBlocks.DOLOMITE_LAYERED_SLAB.get()));
+                block -> createSlabItemTable(ModBlocks.WEATHERED_LIMESTONE_LAYERED_SLAB.get()));
         // GABBRO
         this.add(ModBlocks.GABBRO_SLAB.get(),
                 block -> createSlabItemTable(ModBlocks.GABBRO_SLAB.get()));
-        this.add(ModBlocks.GABBRO_PAVED_BRICK_SLAB.get(),
-                block -> createSlabItemTable(ModBlocks.GABBRO_PAVED_BRICK_SLAB.get()));
         this.add(ModBlocks.GABBRO_COBBLESTONE_SLAB.get(),
                 block -> createSlabItemTable(ModBlocks.GABBRO_COBBLESTONE_SLAB.get()));
         this.add(ModBlocks.GABBRO_POLISHED_SLAB.get(),
@@ -99,7 +93,6 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         //Stair
         // Dolomite
         this.dropSelf(ModBlocks.DOLOMITE_STAIRS.get());
-        this.dropSelf(ModBlocks.DOLOMITE_LAYERED_STAIRS.get());
         this.dropSelf(ModBlocks.DOLOMITE_POLISHED_STAIRS.get());
         this.dropSelf(ModBlocks.DOLOMITE_COBBLESTONE_STAIRS.get());
         this.dropSelf(ModBlocks.DOLOMITE_BRICK_STAIRS.get());
@@ -107,7 +100,6 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         this.dropSelf(ModBlocks.DOLOMITE_PAVED_BRICK_STAIRS.get());
         // Limestone
         this.dropSelf(ModBlocks.WEATHERED_LIMESTONE_STAIRS.get());
-        this.dropSelf(ModBlocks.WEATHERED_LIMESTONE_LAYERED_STAIRS.get());
         this.dropSelf(ModBlocks.WEATHERED_LIMESTONE_POLISHED_STAIRS.get());
         this.dropSelf(ModBlocks.WEATHERED_LIMESTONE_COBBLESTONE_STAIRS.get());
         this.dropSelf(ModBlocks.WEATHERED_LIMESTONE_BRICK_STAIRS.get());
@@ -115,7 +107,6 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         this.dropSelf(ModBlocks.WEATHERED_LIMESTONE_PAVED_BRICK_STAIRS.get());
         // GABBRO
         this.dropSelf(ModBlocks.GABBRO_STAIRS.get());
-        this.dropSelf(ModBlocks.GABBRO_LAYERED_STAIRS.get());
         this.dropSelf(ModBlocks.GABBRO_POLISHED_STAIRS.get());
         this.dropSelf(ModBlocks.GABBRO_COBBLESTONE_STAIRS.get());
         this.dropSelf(ModBlocks.GABBRO_BRICK_STAIRS.get());
@@ -125,7 +116,6 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         // Wall
         // Dolomite
         this.dropSelf(ModBlocks.DOLOMITE_BRICK_WALL.get());
-        this.dropSelf(ModBlocks.DOLOMITE_LAYERED_WALL.get());
         this.dropSelf(ModBlocks.DOLOMITE_WALL.get());
         this.dropSelf(ModBlocks.DOLOMITE_POLISHED_WALL.get());
         this.dropSelf(ModBlocks.DOLOMITE_PAVED_BRICK_WALL.get());
@@ -133,7 +123,6 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         this.dropSelf(ModBlocks.DOLOMITE_COBBLESTONE_WALL.get());
         // Limestone
         this.dropSelf(ModBlocks.WEATHERED_LIMESTONE_BRICK_WALL.get());
-        this.dropSelf(ModBlocks.WEATHERED_LIMESTONE_LAYERED_WALL.get());
         this.dropSelf(ModBlocks.WEATHERED_LIMESTONE_WALL.get());
         this.dropSelf(ModBlocks.WEATHERED_LIMESTONE_POLISHED_WALL.get());
         this.dropSelf(ModBlocks.WEATHERED_LIMESTONE_PAVED_BRICK_WALL.get());
@@ -141,7 +130,6 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         this.dropSelf(ModBlocks.WEATHERED_LIMESTONE_COBBLESTONE_WALL.get());
         // GABBRO
         this.dropSelf(ModBlocks.GABBRO_BRICK_WALL.get());
-        this.dropSelf(ModBlocks.GABBRO_LAYERED_WALL.get());
         this.dropSelf(ModBlocks.GABBRO_WALL.get());
         this.dropSelf(ModBlocks.GABBRO_POLISHED_WALL.get());
         this.dropSelf(ModBlocks.GABBRO_PAVED_BRICK_WALL.get());
